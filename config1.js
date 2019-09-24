cec3c5937ce2e1826b4fe9cad0f8aacf192a3339
Waleed Hanna <dd352426@gmail.com>
2019-09-24 19:18:55 -0400

    Update

---
+++ config1.js
@@ -0,0 +1,2 @@
+// API key
+const API_KEY = "pk.eyJ1Ijoia3VzaHAiLCJhIjoiY2swOW1oNzkzMGFiazNpbXFrZm9zN25iOCJ9.yXiI784QG_X_aWhILP294A";
---
+++ config2.js
@@ -0,0 +1,2 @@
+// API key
+const API_KEY = "pk.eyJ1IjoidHR1cmsiLCJhIjoiY2swOW1oOWJjMGFkbzNtdWJseXRhOTF1bSJ9.2z6kcQ81DS5A0vrdZCqEkA";
